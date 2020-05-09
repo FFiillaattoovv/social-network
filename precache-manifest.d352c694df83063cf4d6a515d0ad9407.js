d094bbf176cb72d26fbb4de64280a20e522bc4f0
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-09 07:20:11 -0700

    Updates

---
+++ precache-manifest.d352c694df83063cf4d6a515d0ad9407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a555894fdada8efa89fbe05fe59d72bb",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "f1d750da691aa2af66bd",
+    "url": "/social-network/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/social-network/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-network/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d750da691aa2af66bd",
+    "url": "/social-network/static/js/main.bdf893a7.chunk.js"
+  },
+  {
+    "revision": "2dea08a3500d8b094a24",
+    "url": "/social-network/static/js/runtime-main.619f7d7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
