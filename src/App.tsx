727e7bf953612d31df2c6971ab6ff1801a264717
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-13 08:48:33 -0700

    Add nav

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,13 @@ function App() {
             <header>
                 <img src="https://th.bing.com/th/id/OIP.S7Q8-xfE09rM1VOcP7gLzwHaIy?w=163&h=192&c=7&o=5&dpr=1.4&pid=1.7"/>
             </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
         </div>
     );
 }
