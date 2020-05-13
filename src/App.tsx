9e3191ded1dc3e22ef5b3365e2fb82cdcfd2cc45
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-13 08:48:13 -0700

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="">
+            <header>
+                <img src="https://th.bing.com/th/id/OIP.S7Q8-xfE09rM1VOcP7gLzwHaIy?w=163&h=192&c=7&o=5&dpr=1.4&pid=1.7"/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
