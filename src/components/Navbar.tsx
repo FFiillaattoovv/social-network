46a36e2a7747514643607847b7fbd40d2dd2701a
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-14 05:37:36 -0700

    Add class active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./Navbar.module.css";
 
+console.log(classes)
+
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a href="">Profile</a></div>
+            <div className={`${classes.item} + " " + ${classes.active}`}><a href="">Profile</a></div>
             <div className={classes.item}><a href="">Messages</a></div>
             <div className={classes.item}><a href="">News</a></div>
             <div className={classes.item}><a href="">Music</a></div>
