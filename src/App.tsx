48e8fa0ce6667bf2c143fae58705b8943e1ce7f5
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-13 23:54:20 -0700

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="app-wrapper">
             <header className="header">
-                <img src="https://th.bing.com/th/id/OIP.S7Q8-xfE09rM1VOcP7gLzwHaIy?w=163&h=192&c=7&o=5&dpr=1.4&pid=1.7"/>
+                <img src="https://th.bing.com/th/id/OIP.5RuWHDPkCpCa6_CKz6jszQHaHa?w=166&h=166&c=7&o=5&dpr=1.4&pid=1.7"/>
             </header>
             <nav className="nav">
                 <div><a href="">Profile</a></div>
@@ -15,7 +15,16 @@ function App() {
                 <div><a href="">Settings</a></div>
             </nav>
             <div className="content">
-                Main content
+                <img src="https://cdn.pixabay.com/photo/2016/10/29/23/48/animals-1782013__340.png" alt=""/>
+                <div>
+                    Ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
             </div>
         </div>
     );
