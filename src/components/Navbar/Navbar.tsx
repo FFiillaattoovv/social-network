c51ed9082bb576bd1627c3d1ee2e181bfc6aa5ef
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-15 10:36:02 -0700

    Change the tag "a" to "NavLink"

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} + " " + ${classes.active}`}><a href="/profile">Profile</a></div>
-            <div className={classes.item}><a href="/dialogs">Messages</a></div>
+
+            <div className={classes.item}><NavLink to="/profile">Profile</NavLink></div>
+            <div className={classes.item}><NavLink to="/dialogs">Messages</NavLink></div>
             <div className={classes.item}><a href="">News</a></div>
             <div className={classes.item}><a href="">Music</a></div>
             <div className={classes.item}><a href="">Settings</a></div>
