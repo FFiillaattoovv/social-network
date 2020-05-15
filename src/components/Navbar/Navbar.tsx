0e7bcdcc1a086fe88939122a492175484188fb04
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-15 09:29:39 -0700

    Add react-router-dom

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import classes from "./Navbar.module.css";
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} + " " + ${classes.active}`}><a href="">Profile</a></div>
-            <div className={classes.item}><a href="">Messages</a></div>
+            <div className={`${classes.item} + " " + ${classes.active}`}><a href="/profile">Profile</a></div>
+            <div className={classes.item}><a href="/dialogs">Messages</a></div>
             <div className={classes.item}><a href="">News</a></div>
             <div className={classes.item}><a href="">Music</a></div>
             <div className={classes.item}><a href="">Settings</a></div>
