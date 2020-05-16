5a259420266ef636bf6df0cc5ead51fb0269fdb6
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-16 06:19:51 -0700

    Adds structure in dialogs and style

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,7 +3,20 @@ import classes from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + " " + classes.active}>Andrey</div>
+                <div className={classes.dialog}>Maria</div>
+                <div className={classes.dialog}>Maxim</div>
+                <div className={classes.dialog}>Paul</div>
+                <div className={classes.dialog}>Elena</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>I'm fine!</div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -4,7 +4,7 @@ import classes from "./Settings.module.css"
 const Settings = () => {
     return (
         <div className={classes.content}>
-            <img src="https://th.bing.com/th/id/OIP.0PEeHJzo651BhsRKH21yFQHaDt?w=300&h=150&c=7&o=5&dpr=1.4&pid=1.7" alt=""/>
+            <img src="https://th.bing.com/th/id/OIP.Rm_nlEISTUnu2Pd3n4PM5wHaHa?w=232&h=216&c=7&o=5&dpr=1.4&pid=1.7" alt=""/>
         </div>
     )
 }
