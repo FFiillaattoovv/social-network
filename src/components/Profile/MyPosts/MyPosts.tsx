274039fa6b316a7d65b7f3282161a26dc5833ec6
FFiillaattoovv <admirator2015@yandex.ru>
2020-05-20 02:49:36 -0700

    Data postData (Array <object>)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,16 +3,21 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 4},
+        {id: 2, message: "It's my first post.", likesCount: 16}
+    ];
+
     return (
         <div>
             <div>
                 <h3>My posts</h3>
                 <textarea></textarea>
-
             </div>
             <div>New post</div>
-            <Post message="Hi, how are you?" likesCount="4" />
-            <Post message="It's my first post." likesCount="16" />
+            <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+            <Post message={postData[1].message} likesCount={postData[1].likesCount} />
         </div>
     )
 }
