dc38a8bb7e735e6e5ea1dc01ce5103e1c5e59be9
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-09 04:28:04 -0700

    Adds function addPost, newPostElement (React.createRef<HTMLTextAreaElement>()), button (onClick) in MyPosts

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {PostType} from "../../redux/state";
+import addPost, {PostType, addPostType} from "../../redux/state";
 
 type PropsType = {
     posts: Array<PostType>
+    addPost: addPostType
 }
 
 const Profile = (props: PropsType) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={props.posts}/>
+            <MyPosts posts={props.posts} addPost={props.addPost}/>
         </div>
     )
 }
