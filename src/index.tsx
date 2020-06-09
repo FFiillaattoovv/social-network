48daac22e7df50dc4d56363e37f5260b0d4338db
FFiillaattoovv <admirator2015@yandex.ru>
2020-06-09 07:08:28 -0700

    Adds function renderEntireTree in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,16 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+let renderEntireTree = () => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
