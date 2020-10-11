99a4d589ce177aa96a5ee6c7349001b078122118
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-11 10:20:02 -0700

    Fix errors

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -2,13 +2,11 @@ import {PostType} from "./state";
 
 const profileReducer = (state, action) => {
     if (action.type === "ADD-POST") {
-        let newPost: PostType = {id: 3, message: this._state.profilePage.newPostText, likesCount: 0};
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = "";
-        this._callSubscriber();
+        let newPost: PostType = {id: 3, message: state.newPostText, likesCount: 0};
+        state.posts.push(newPost);
+        state.newPostText = "";
     } else if (action.type === "UPDATE-NEW-POST-TEXT") {
-        this._state.profilePage.newPostText = action.newText;
-        this._callSubscriber();
+        state.newPostText = action.newText;
     }
     return state;
 }
