db0f359a43c521682042eaacaa5b5feb8e636a08
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-14 10:48:38 -0700

    Transfer logic to StoreContext.Consumer

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -12,24 +12,26 @@ type PropsType = {
 
 const MyPostsContainer = (props: PropsType) => {
 
-    let state = props.store.getState()
-
-    let addPost = () => {
-        props.dispatch(addPostActionCreator());
-    }
-
-    let onPostChange = (text) => {
-        let action = updateNewPostTextActionCreator(text)
-        props.dispatch(action);
-    }
-
     return (
-        <StoreContext.Consumer>
-            (store) => (<MyPosts updateNewPostText={onPostChange}
-                            addPost={addPost}
-                            posts={state.profilePage.posts}
-                            newPostText{state.profilePage.newPostText}/>)
-        </StoreContext.Consumer>
+        <StoreContext.Consumer> {
+            (store) => {
+
+                let state = props.store.getState();
+
+                let addPost = () => {
+                    props.store.dispatch(addPostActionCreator());
+                }
+
+                let onPostChange = (text) => {
+                    let action = updateNewPostTextActionCreator(text)
+                    props.store.dispatch(action);
+                }
+                return <MyPosts updateNewPostText={onPostChange}
+                                addPost={addPost}
+                                posts={state.profilePage.posts}
+                                newPostText{state.profilePage.newPostText}/>
+            }
+        } </StoreContext.Consumer>
     )
 }
 export default MyPostsContainer;
