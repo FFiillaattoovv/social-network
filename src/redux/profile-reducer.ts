49adba0557d4f98e811974c8256d49d8492bccf6
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-20 11:31:51 -0700

    Code refactoring(add copy state in profileReducer).

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -10,14 +10,19 @@ let initialState = {
 
 const profileReducer = (state: ProfilePageType = initialState, action: ActionsTypes) => {
     switch (action.type) {
-        case "ADD-POST":
+        case "ADD-POST": {
             let newPost: PostType = {id: 3, message: state.newPostText, likesCount: 0};
-            state.posts.push(newPost);
-            state.newPostText = "";
-            return state;
-        case "UPDATE-NEW-POST-TEXT":
-            state.newPostText = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = "";
+            return stateCopy;
+        }
+        case "UPDATE-NEW-POST-TEXT": {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
