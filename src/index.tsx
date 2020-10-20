19331792f2378bacfc02c3fef0df02260ac00236
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-20 11:13:38 -0700

    Delete wrapper renderEntireTree in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import App from "./App";
 import store from "./redux/redux-store";
 import {Provider} from "react-redux";
 
-let renderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
@@ -18,7 +17,6 @@ let renderEntireTree = () => {
         </React.StrictMode>,
         document.getElementById('root')
     );
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
