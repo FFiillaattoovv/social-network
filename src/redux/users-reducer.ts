dbb135004f76f5d7a28c199af0c3ce3c3767b617
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-22 03:35:55 -0700

    Add logic for followAC and unfollowAC in usersReducer

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -26,7 +26,33 @@ let initialState = {
 
 
 const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FOLLOW":
+            return {
+                ...state,
+                users: [...state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: true}
+                    }
+                    return u;
+                })
+                ]
+            };
+        case "UNFOLLOW":
+            return {
+                ...state,
+                users: [...state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+                ]
+            };
 
+        default:
+            return state;
+    }
 }
 
 export const followAC = (userId) => ({type: "FOLLOW", userId})
