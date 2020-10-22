22a5a1d821d5a974cb670bb2bddcc1d99b7be4b7
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-22 05:48:27 -0700

    Add callback for button in Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,10 +8,14 @@ let Users = (props) => {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={u.photoUrl} className={styles.userPhoto} alt="User avatar"/>
                         </div>
                         <div>
-                            <button>Follow</button>
+                            {u.followed ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }}>Unfollow</button> : <button onClick={() => {
+                                props.follow(u.id)
+                            }}>Follow</button>}
                         </div>
                     </span>
                     <span>
