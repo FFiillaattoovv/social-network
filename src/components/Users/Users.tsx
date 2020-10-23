7d61c0efa2c142d3a891cdec682ad769c2b1bcf1
FFiillaattoovv <admirator2015@yandex.ru>
2020-10-23 01:01:16 -0700

    Fix bug fir update setUsers in Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,34 +2,36 @@ import React from "react";
 import styles from "./users.module.css";
 
 let Users = (props) => {
-    props.setUsers(
-        [
-            {
-                id: 1,
-                photoUrl: "https://pbs.twimg.com/profile_images/1236759527179124738/pEx2QkKw_400x400.jpg",
-                followed: false,
-                fullName: "Dmitry",
-                status: "I am a boss",
-                location: {citi: "Minsk", country: "Belarus"}
-            },
-            {
-                id: 2,
-                photoUrl: "https://pbs.twimg.com/profile_images/1062368194118197248/82fZyeo_.jpg",
-                followed: true,
-                fullName: "Sasha",
-                status: "I am a boss too",
-                location: {citi: "Moscow", country: "Russia"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://pbs.twimg.com/profile_images/1102796114741227521/YiJ4QEAF_400x400.png",
-                followed: false,
-                fullName: "Andrew",
-                status: "I am a boss too too",
-                location: {citi: "Kiev", country: "Ukraine"}
-            }
-        ]
-    );
+    if (props.users.length === 0) {
+        props.setUsers(
+            [
+                {
+                    id: 1,
+                    photoUrl: "https://pbs.twimg.com/profile_images/1236759527179124738/pEx2QkKw_400x400.jpg",
+                    followed: false,
+                    fullName: "Dmitry",
+                    status: "I am a boss",
+                    location: {citi: "Minsk", country: "Belarus"}
+                },
+                {
+                    id: 2,
+                    photoUrl: "https://pbs.twimg.com/profile_images/1062368194118197248/82fZyeo_.jpg",
+                    followed: true,
+                    fullName: "Sasha",
+                    status: "I am a boss too",
+                    location: {citi: "Moscow", country: "Russia"}
+                },
+                {
+                    id: 3,
+                    photoUrl: "https://pbs.twimg.com/profile_images/1102796114741227521/YiJ4QEAF_400x400.png",
+                    followed: false,
+                    fullName: "Andrew",
+                    status: "I am a boss too too",
+                    location: {citi: "Kiev", country: "Ukraine"}
+                }
+            ]
+        );
+    }
     return (
         <div>
             {
