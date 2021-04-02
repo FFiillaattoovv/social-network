9a911a0f2fd6eafd45b47929fcc5552cd4ef172d
FFiillaattoovv <admirator2015@yandex.ru>
2021-04-02 14:53:14 +0300

    Delete UsersAPIComponent.tsx and transfer  logics to UserContainer

--- src/components/Users/UsersContainer.ts
+++ src/components/Users/UsersContainer.ts
@@ -1,9 +1,10 @@
 import {connect} from 'react-redux';
-import {UserType} from './Users';
+import Users, {UserType} from './Users';
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from '../../redux/users-reducer';
 import {AppStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
-import UsersAPIComponent from './UsersAPIComponent';
+import React from 'react';
+import axios from 'axios';
 
 type MapStatePropsType = {
     users: Array<UserType>,
@@ -17,6 +18,56 @@ type MapDispatchPropsType = {
     setUsers: (users: Array<UserType>) => void
     setCurrentPage: (currentPage: number) => void
     setTotalUsersCount: (totalCount: number) => void
+}
+
+export type UserType = {
+    id: number
+    photos: { small: string, large: string }
+    followed: boolean
+    name: string
+    status: string
+    location: LocationType
+}
+
+type LocationType = {
+    citi: string
+    country: string
+}
+
+type PropsType = {
+    users: Array<UserType>
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (users: Array<UserType>) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalUsersCount: (totalCount: number) => void
+    totalUserCount: number
+    pageSize: number
+    currentPage: number
+}
+
+class UsersAPIComponent extends React.Component<PropsType> {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <Users users={this.props.users} follow={this.props.follow} unfollow={this.props.unfollow}
+        onPageChanged={this.onPageChanged}
+        totalUserCount={this.props.totalUserCount} pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}/>
+    }
 }
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
