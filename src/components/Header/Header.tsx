614f1bb69674fcf8a47bf1af2fbd9a50a91a8884
FFiillaattoovv <admirator2015@yandex.ru>
2021-04-12 14:19:48 +0300

    Adds NavLink for login and style in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import classes from "./Header.module.css";
+import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className={classes.header}>
-            <img src="https://th.bing.com/th/id/OIP.5RuWHDPkCpCa6_CKz6jszQHaHa?w=166&h=166&c=7&o=5&dpr=1.4&pid=1.7"/>
+            <img src="https://th.bing.com/th/id/OIP.5RuWHDPkCpCa6_CKz6jszQHaHa?w=166&h=166&c=7&o=5&dpr=1.4&pid=1.7"
+                 alt="header"/>
+            <div className={classes.loginBlock}>
+                <NavLink to={'/login'}>login</NavLink>
+            </div>
         </header>
     )
 }
