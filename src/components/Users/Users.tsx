0fc66eb33fa8ea9b0724e41967de944d53ef99d7
FFiillaattoovv <admirator2015@yandex.ru>
2021-04-20 13:06:48 +0300

    Adds call for button in Users component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -86,13 +86,9 @@ const Users = (props: PropsType) => {
                         <div>
                             {
                                 u.followed ? <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                                     onClick={() => {
-
-                                                     }}>Unfollow</button> :
-                                    <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                            onClick={() => {
-
-                                            }}>Follow</button>
+                                                     onClick={() => {props.unfollow(u.id)}}>Unfollow</button> :
+                                             <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                                     onClick={() => {props.follow(u.id)}}>Follow</button>
                             }
                         </div>
                     </span>
