39609eda7bebe6d7c611d1374e764f720cd7c951
FFiillaattoovv <admirator2015@yandex.ru>
2021-04-20 11:01:18 +0300

    Add thunkMiddleware

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,6 +3,7 @@ import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
+import thunkMiddleware from 'redux-thunk';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -15,6 +16,6 @@ type RootReducerType = typeof reducers;
 
 export type AppStateType = ReturnType<RootReducerType>;
 
-let store = createStore(reducers, applyMiddleware());
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
