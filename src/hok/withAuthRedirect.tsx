a3fbb18c780da8fc5248132efb7f5964e245514b
FFiillaattoovv <admirator2015@yandex.ru>
2021-05-13 12:02:16 +0300

    Adds logics to withAuthRedirect

--- src/hok/withAuthRedirect.tsx
+++ src/hok/withAuthRedirect.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {AppStateType} from '../redux/redux-store';
+
+type mapStateToPropsType = {
+    isAuth: boolean
+}
+
+const mapStateToProps = (state: AppStateType): mapStateToPropsType => {
+    return {
+        isAuth: state.auth.isAuth
+    }
+}
 
 export const withAuthRedirect = (Component: any) => {
-    class RedirectComponent extends React.Component {
+    class RedirectComponent extends React.Component<mapStateToPropsType> {
         render() {
-            if(!this.props.isAuth) return <Redirect to='/login'/>;
+            if (!this.props.isAuth) return <Redirect to="/login"/>;
             return <Component {...this.props}/>;
         }
     }
 
-    return RedirectComponent;
+    let ConnectRedirectComponent = connect(mapStateToProps)(RedirectComponent);
+
+    return ConnectRedirectComponent;
 }
