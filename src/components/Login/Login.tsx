eb331cd6880c185fe9ce4c2a107fe76bbd3365ae
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-13 16:28:42 +0300

    Adds types for LoginForm component and reduxForm

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 
 const Login = () => {
     return (
         <div>
             <h1>Login</h1>
-            <LoginForm/>
+            <LoginReduxForm/>
         </div>
     )
 }
 
-const LoginForm = () => {
+type FormDataType = {
+    login: string
+    password: string
+    rememberMy: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = () => {
     return (
-        <form action="">
-            <div><input type="text" placeholder={'Login'}/></div>
-            <div><input type="text" placeholder={'Password'}/></div>
-            <div><input type="checkbox"/>remember me</div>
+        <form>
+            <div><Field type="text" placeholder={'Login'} name={'login'} component={'input'}/></div>
+            <div><Field type="text" placeholder={'Password'} name={'password'} component={'input'}/></div>
+            <div><Field type="checkbox" name={'rememberMy'} component={'input'}/>remember me</div>
             <div>
                 <button>Login</button>
             </div>
         </form>
     )
 }
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    form: 'login'
+})(LoginForm);
 
 export default Login;
