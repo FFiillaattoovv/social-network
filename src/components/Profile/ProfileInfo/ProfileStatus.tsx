073eb52fdb990ee50babd44f093eb20c911b97fb
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-13 14:28:27 +0300

    Fix problem with update status

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -11,6 +11,14 @@ class ProfileStatus extends React.Component<PropsType> {
         status: this.props.status
     }
 
+    componentDidUpdate(prevProps: Readonly<PropsType>, prevState: Readonly<{}>, snapshot?: any) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     activateEditMode = (): void => {
         this.setState({
             editMode: true
