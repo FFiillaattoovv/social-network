bb099ae90500e5ae8c0f31e1b83691fdea1420a5
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-13 15:45:37 +0300

    Create LoginForm for Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <h1>
-            Please login
-        </h1>
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
+
+const LoginForm = () => {
+    return (
+        <form action="">
+            <div><input type="text" placeholder={'Login'}/></div>
+            <div><input type="text" placeholder={'Password'}/></div>
+            <div><input type="checkbox"/>remember me</div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
 
