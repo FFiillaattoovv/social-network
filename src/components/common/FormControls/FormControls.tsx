e57845532fbdc94fb738081868502f16ea542d7c
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-15 18:35:01 +0300

    Create FormControl and refactoring

--- src/components/common/FormControls/FormControls.tsx
+++ src/components/common/FormControls/FormControls.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 import {WrappedFieldProps} from 'redux-form';
 import styles from './FormControls.module.css';
 
-export const Textarea: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
+const FormControl: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
-        <div className={styles.formControl + " " + (hasError ? styles.error: '')}>
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
             <div>
-                <textarea {...input} {...props}/>
+                {props.children}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 };
 
-export const Input: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={styles.formControl + " " + (hasError ? styles.error: '')}>
-            <div>
-                <input {...input} {...props}/>
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+};
+
+export const Input: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
 };
