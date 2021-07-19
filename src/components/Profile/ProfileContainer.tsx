1c2565ecae1af1d34ec0f8c54124dfa98d345ceb
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-19 17:21:35 +0300

    Add redirect to login if user not authorized

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -40,6 +40,9 @@ class ProfileContainer extends React.Component<PropsType> {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
+            if (!userId) {
+                this.props.history.push('/login');
+            }
         }
         this.props.getUserProfileThunkCreator(userId);
         this.props.getUserStatusThunkCreator(userId);
