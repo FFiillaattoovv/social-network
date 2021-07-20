69808b2b5e93753680947599329bd2c588162ed5
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-20 12:53:59 +0300

    Add connect for Redux DevTools Extension

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form';
+import {reducer as formReducer} from 'redux-form';
 import appReducer from './app-reducer';
 
 let reducers = combineReducers({
@@ -20,6 +20,14 @@ type RootReducerType = typeof reducers;
 
 export type AppStateType = ReturnType<RootReducerType>;
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
 
 export default store;
