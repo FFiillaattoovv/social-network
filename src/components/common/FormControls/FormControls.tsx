7bc1c8f86a506f6027537246fa4bf9108f41e00f
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-21 15:48:30 +0300

    Refactoring FormControls component

--- src/components/common/FormControls/FormControls.tsx
+++ src/components/common/FormControls/FormControls.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {WrappedFieldProps} from 'redux-form';
 import styles from './FormControls.module.css';
 
-const FormControl: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error;
+const FormControl: React.FC<WrappedFieldProps> = ({ meta: {touched, error}, children}) => {
+    const hasError = touched && error;
     return (
         <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
             <div>
-                {props.children}
+                {children}
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <span>{error}</span>}
         </div>
     )
 };
