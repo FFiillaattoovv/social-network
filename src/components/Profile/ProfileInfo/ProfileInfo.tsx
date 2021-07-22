be245e77e874796057eb2b51d6d7908efaaaf410
FFiillaattoovv <admirator2015@yandex.ru>
2021-07-22 19:26:29 +0300

    Add default userPhoto in ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import classes from './ProfileInfo.module.css';
 import {ProfileType} from '../../../redux/profile-reducer';
 import Preloader from '../../common/Preloader/Preloader';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import userPhoto from '../../../assets/images/images.png';
 
 type PropsType = {
     profile: ProfileType | null
@@ -19,7 +20,7 @@ const ProfileInfo = ({profile, status, updateStatus}: PropsType) => {
             <div className={classes.content}>
                 <img src="https://cdn.pixabay.com/photo/2016/10/29/23/48/animals-1782013__340.png" alt=""/>
             </div>
-            <img src={profile.photos.large} alt="profile"/>
+            <img src={profile.photos.large || userPhoto} alt="profile"/>
             <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
     )
